7d5789b2ca62e9a4f3b27827bfd0b34fde557877
Rk-Virus <ravim70655@gmail.com>
2026-02-09 11:15:34 +0530

    added markup schema for crawlers

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,33 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
+// schema for crawlers to show when searched on google
+const schemaObject = {
+  "@context": "https://schema.org/",
+  "@type": "Person",
+  "name": "Ravi Mishra",
+  "url": "https://rkvirus.vercel.app",
+  "image": "https://rkvirus.vercel.app/imgs/avatar.jpg",
+  "sameAs": [
+    "https://www.instagram.com/itz_rkvirus",
+    "https://www.linkedin.com/in/rkvirus/",
+    ""
+  ],
+  "jobTitle": "Full Stack Developer"
+}
+
 export default function Document() {
   return (
     <Html>
       <Head>
         {/* <link rel="stylesheet" href="" /> */}
         {/* <!-- CSS only --> */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(schemaObject),
+          }}
+        />
       </Head>
       <body id="home">
         <div id="fb-root"></div>
